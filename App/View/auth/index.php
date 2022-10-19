c20a1e8d5af022098b048ff5ebc66f5b2d8fdd9a
tucno21 <carlitostucno@gmail.com>
2022-10-18 20:51:43 -0500

    cambio de denominacion

--- App/View/auth/index.php
+++ App/View/auth/index.php
@@ -2,7 +2,7 @@
 
 <div class="vh-100 d-flex justify-content-center align-items-center">
     <div class="col-md-4 p-5 shadow-sm border rounded-5 border-primary">
-        <h4 class="text-center mb-4 text-light">Elecciones Municipales Escolares 2023</h4>
+        <h4 class="text-center mb-4 text-light">Elecciones del Municipio Escolar 2023</h4>
         <p class="text-center mb-4 text-light">¡Acceso a voto!</p>
         <?php
         if (session()->has('errorSesion')) {
